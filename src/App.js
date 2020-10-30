797367f0d8327496c14044bffc11ffafb158ca2f
Insomnia <jamie.mar.jones14@gmail.com>
2020-10-29 20:35:15 -0400

    trying to get employees to show

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="header-section">
+          <h1>Employee Directory</h1>
+          <p>
+            Click on carrots to filter by heading or use the search box to
+            narrow your results
+          </p>
+        </section>
+        <section className="input-section">
+          <input
+            type="search"
+            class="form-control search-input"
+            placeholder="Search"
+          ></input>
+        </section>
       </header>
     </div>
   );
